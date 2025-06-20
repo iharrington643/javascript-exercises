0766e14f2c50916cae1f643b77c2edac43b60821
Isabella Harrington <isabellaharrington643@gmail.com>
2025-06-20 10:43:58 -0400

    Added numbers between and including first/lastNum
    Checked if first/lastNum were valid prior

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(firstNum, lastNum) {
+    finalSum = 0;
 
+    if (firstNum < 0 || lastNum < 0 || !Number.isInteger(firstNum) || !Number.isInteger(lastNum)) {
+        return "ERROR";
+    }
+
+    if (firstNum < lastNum) {
+        for (let i = firstNum; i <= lastNum; i++) {
+            finalSum += i;
+        }
+    } else {
+        for (let i = firstNum; i >= lastNum; i--) {
+            finalSum += i;
+        }
+    }
+
+    return finalSum;
 };
 
 // Do not edit below this line
