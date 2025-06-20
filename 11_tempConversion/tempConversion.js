cd74bf90b754008361730771d2a1e8ce46322529
Isabella Harrington <isabellaharrington643@gmail.com>
2025-06-20 13:09:31 -0400

    Used mathematical operators and math module to create functions
    These functions take input for temperature
    and convert it to Celsius or Fahrenheit

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  const newTempC = (tempF - 32) * (5/9);
+  return Math.round(newTempC * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  const newTempF = (tempC * 9/5) + 32;
+  return Math.round(newTempF * 10) / 10;
 };
+
+console.log(convertToCelsius(32));
 
 // Do not edit below this line
 module.exports = {
