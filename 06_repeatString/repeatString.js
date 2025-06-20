8f162b11b091ffd59748b3954c76992715078b8b
Isabella Harrington <isabellaharrington643@gmail.com>
2025-06-20 09:53:59 -0400

    Created a newString variable for the new newString
    Added each string to newString a given # of times

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,4 +1,11 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+
+    if (num < 0) return "ERROR";
+    let newString = "";
+    for (let i = 0; i < num; i++) {
+        newString = newString + string;
+    }
+    return newString;
 
 };
 
