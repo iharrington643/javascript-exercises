71d9a0931b72ca4aca546e86474e8805275ec54a
Isabella Harrington <isabellaharrington643@gmail.com>
2025-06-29 15:34:19 -0400

    Used basic mathematical operators and for loops to create calculator functions
    Tested all functions with spec file

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(add1, add2) {
+	return add1 + add2;
 };
 
-const subtract = function() {
-	
+const subtract = function(subtract1, subtract2) {
+	return subtract1 - subtract2;
 };
 
-const sum = function() {
-	
+const sum = function(sumArray) {
+	total = 0;
+
+  for (i = 0; i < sumArray.length; i++) {
+    total = total + sumArray[i];
+  };
+
+  return total;
 };
 
-const multiply = function() {
+const multiply = function(multiplyArray) {
+  total = 1;
 
+  for (i = 0; i < multiplyArray.length; i++) {
+    total = total * multiplyArray[i];
+  };
+
+  return total;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  total = 1;
+
+  for (i = 0; i < n; i++) {
+    total = total * (i + 1);
+  };
+
+  return total;
 };
 
 // Do not edit below this line
