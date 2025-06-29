3f1b7f71b7f4cddca62ed256d5da231c7a5936c8
Isabella Harrington <isabellaharrington643@gmail.com>
2025-06-29 17:57:18 -0400

    Used functions split, reverse, replace, and join to reverse strings and check for palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,4 +1,14 @@
-const palindromes = function () {
+const palindromes = function (testString) {
+    
+    let cleanedString = testString.replace(/[\s,.!]/g, '');
+    cleanedString = cleanedString.toLowerCase();
+    let reversedString = cleanedString.split('').reverse().join('');
+    
+    if (reversedString == cleanedString) {
+        return true;
+    } else {
+        return false;
+    };
 
 };
 
