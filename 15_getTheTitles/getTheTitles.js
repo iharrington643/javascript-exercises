619a54be49f7acc82832a59900d94781008efaa6
Isabella Harrington <isabellaharrington643@gmail.com>
2025-06-29 21:13:40 -0400

    Used object references and for of loop to find all values associated with the key 'title' within array objects

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    const titles = [];
+    for (const obj of array) {
+        if ('title' in obj) {
+            titles.push(obj.title);
+        };
+    }
+    return titles;
 };
 
 // Do not edit below this line
