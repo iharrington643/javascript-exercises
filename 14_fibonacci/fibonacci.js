ec96368af8ccd193ca18195d82b2a6cac498352d
Isabella Harrington <isabellaharrington643@gmail.com>
2025-06-29 21:01:43 -0400

    Used for loop with values a and b to calculate the values at certain index of Fibonacci sequence

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    if (index == 0) return 0;
+    if (index == 1) return 1;
+    if (index < 0) return "OOPS";
 
+    a = 0;
+    b = 1;
+
+    for (i = 2; i <= index; i++) {
+        let next = a + b;
+        a = b;
+        b = next;
+    }
+
+    return b;
 };
 
 // Do not edit below this line
